8291963fe563f02bb00aa0d3c8e989131b319a31
dkf-lab <pxlbuisness@gmail.com>
2020-07-01 20:54:06 +0930

    Added scanning support

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from playsound import playsound
 import glob
+import os
 # find sounds in directory and put them in list
-avaliableSounds = [f for f in glob.glob("*.mp3")] #add MP3 sounds to list
+avaliableSounds = []
+def scanDir():
+    global avaliableSounds
+    avaliableSounds = [f for f in glob.glob("*.mp3")] #add MP3 sounds to list
+scanDir()
+clear = lambda: os.system('cls')
 
 while True:
-    print(''*3)
+    clear()
+    print("Type 'exit' to exit or 'rescan' to scan for new files.")
     print('Avaliable Sounds:')
     count = 0
     for x in avaliableSounds:
         print(count, x)
         count += 1
     uinput = input('Which sound would you like to play? ')
+    if uinput == ('rescan'):
+        scanDir()
     if uinput == ('exit'):
         quit()
     else:
